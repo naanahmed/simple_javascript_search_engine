c2deedbfe0d4c2d4e0b666f944f95b42139c8e07
Naan Ahmed <naanahmed.na@gmail.com>
2022-12-18 23:44:56 +0300

    add javascript

--- search.js
+++ search.js
@@ -1,3 +1,24 @@
+const search = () => {
+    const searchbox = document.getElementById("search-item").value.toUpperCase();
+    const storeitems = document.getElementById("product-list");
+    const product = document.querySelectorAll(".product");
+    const product_name= document.getElementsByTagName("h2");
 
+    for (i=0; i < product_name.length; i++){
+        let match = product[i].getElementsByTagName('h2')[0]
+    }
 
+    if(match ){
+       let textvalue = match.textContent || match.innerHTML
+
+       if(textvalue.toUpperCase().indexof(searchbox) > -1){
+            product[i].style.display = "";
+       }
+       else{
+            product[i].style.display = "none";
+
+       }
+    }
+}
   
+
